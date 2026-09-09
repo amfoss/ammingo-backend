c8bcceda0a5e44f2d6aebab8f68c44b2c94c93af
Kota-Jagadeesh <kota.jagadesh123@gmail.com>
2026-09-10 03:33:38 +0530

    fix: move dot env for correct env loading

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,3 +1,6 @@
+import dotenv
+dotenv.load_dotenv()
+
 from app.db.models import User
 from app.db.db import get_db
 from app.models.auth import UserDetails, EmailLoginRequest, EmailVerify
@@ -8,7 +11,6 @@ from email.message import EmailMessage
 from jose import jwt
 import os
 from authlib.integrations.starlette_client import OAuth
-import dotenv
 import random
 import string
 import smtplib
@@ -16,17 +18,18 @@ from starlette.middleware.sessions import SessionMiddleware
 from datetime import datetime, timezone, timedelta
 
 secret = os.environ.get("JWT_SECRET")
+if not secret:
+    secret = os.environ.get("SECRET_KEY", "amingosupersecretproductionkey2026!@#$%")
 algorithm = os.environ.get("HASH_ALGORITHM", default="HS256")
-expiry_time = int(os.environ.get("TOKEN_EXPIRY_TIME", default="1"))
+expiry_time = int(os.environ.get("TOKEN_EXPIRY_TIME", default="60"))
 email_addr = os.environ.get("EMAIL_ADDRESS")
 email_pass = os.environ.get("EMAIL_PASSWORD")
-smtp_server = os.environ.get("SMTP_SERVER")
-smtp_port = os.environ.get("SMTP_PORT")
+smtp_server = os.environ.get("SMTP_SERVER", "smtp.gmail.com")
+smtp_port = os.environ.get("SMTP_PORT", "587")
 google_client_id = os.environ.get("GOOGLE_CLIENT_ID")
 google_client_secret = os.environ.get("GOOGLE_CLIENT_SECRET")
 frontend_url = os.environ.get("FRONTEND_URL")
 
-dotenv.load_dotenv()
 router = APIRouter()
 oauth = OAuth()
 oauth.register(
